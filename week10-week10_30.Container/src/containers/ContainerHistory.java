99848001cb64702b345df09031850d8b0077f097
Tracy Karjala <tracykarjala@Tracys-MacBook-Air.local>
2019-01-26 22:55:31 -0600

    work on exercise week10_30

--- week10-week10_30.Container/src/containers/ContainerHistory.java
+++ week10-week10_30.Container/src/containers/ContainerHistory.java
@@ -1,4 +1,25 @@
 package containers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContainerHistory {
+    private List<Double> history;
+
+    public ContainerHistory() {
+        this.history = new ArrayList<Double>();
+    }
+
+    public void add(double situation) {
+        history.add(situation);
+    }
+
+    public void reset() {
+        history.clear();
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(history);
+    }
 }
--- week10-week10_30.Container/src/containers/ProductContainer.java
+++ week10-week10_30.Container/src/containers/ProductContainer.java
@@ -1,4 +1,23 @@
 package containers;
 
-public class ProductContainer {
+public class ProductContainer extends Container {
+    private String productName;
+
+    public ProductContainer(String productName, double tilavuus) {
+        super(tilavuus);
+        this.productName = productName;
+    }
+
+    public String getName() {
+        return productName;
+    }
+
+    public void setName(String productName) {
+        this.productName = productName;
+    }
+
+    @Override
+    public String toString() {
+        return productName + ": " + super.toString();
+    }
 }
