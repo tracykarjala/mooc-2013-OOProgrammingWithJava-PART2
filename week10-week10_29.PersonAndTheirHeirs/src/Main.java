e5175d5a34adeee7af6a6ee494dda0a1d0f1be2d
Tracy Karjala <tracykarjala@Tracys-MacBook-Air.local>
2019-01-26 22:33:47 -0600

    Added exercise week10_29

--- week10-week10_29.PersonAndTheirHeirs/src/Main.java
+++ week10-week10_29.PersonAndTheirHeirs/src/Main.java
@@ -1,8 +1,16 @@
+import people.Person;
 
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         // test your code here
+    }
+
+    public static void printDepartment(List<Person> people) {
+        for (Person person : people) {
+            System.out.println(person);
+        }
     }
 }
